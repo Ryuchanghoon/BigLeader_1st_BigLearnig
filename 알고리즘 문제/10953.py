c0d9916056c3345ce09e9f561cf9fd3cfd0fa142
류창훈 <fbckdgns3@naver.com>
2023-06-27 00:19:59 +0900

    [upload] 프로그래머스 '특정 문자열로 끝나는 가장 긴 부분 문자열 찾기' 파이썬 문제풀이 입니다.

---
+++ 알고리즘 문제/10953.py
@@ -0,0 +1,9 @@
+import sys
+
+input = sys.stdin.readline
+
+
+for i in range(int(input())):
+    a,b = map(int,input().split(','))
+
+    print(a + b)
---
+++ 알고리즘 문제/programmers1.py
@@ -0,0 +1,16 @@
+import sys
+
+input = sys.stdin.readline
+
+str = input()
+
+result = ''
+
+for i in str:
+    if i.islower():
+        result += i.upper()
+        
+    elif i.isupper():
+        result += i.lower()
+        
+print(result)
