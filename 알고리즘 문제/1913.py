8dd5dca68862fc027440a0ca4aab086c470f73b3
류창훈 <fbckdgns3@naver.com>
2023-07-15 20:25:28 +0900

    [fix] 백준 1913번 달팽이 문제 수정

--- 알고리즘 문제/1913.py
+++ 알고리즘 문제/1913.py
@@ -2,42 +2,38 @@ import sys
 
 input = sys.stdin.readline
 
-n = int(input())
+n = int(input()) # 7
+number = int(input()) # 35 특정값
+direction =[[1,0],[0,1],[-1,0],[0,-1]] # down, right, up, left
 
-what = int(input())
+snail = [[0 for _ in range(n)] for _ in range(n)]
 
-
-snail = [[0] * n for _ in range(n)] # matrix 생성
-
-x, y = -1, 0 # 현재 위치
-dir = 1 # 이동 방향
-
-
-num = n ** 2
-
+start = n * n # 달팽이가 N^2 시작하는 값이고, 내림차순으로 들어간다
+curr_y = 0
+curr_x = 0
+curr_dir = 0 # 0, 1, 2, 3 까지 들어가야함
+solution = list() # 문제에서 원하는 특정값의 좌표값을 담기 위함
 
 
-while num > 0:
-
-    # X 축
-    for i in range(n): # 숫자 가득 채우고
-        x += dir # 방향(dir)만큼 증가.
-        snail[y][x] = num
-        num -= 1 # 달팽이 움직이고  1개씩 감소.
+for _ in range(n * n):
+    if start == number:
+        solution = [curr_y + 1, curr_x + 1]
+    snail[curr_y][curr_x] = start # 25
+    next_y = curr_y + direction[curr_dir][0] # 1
+    next_x = curr_x + direction[curr_dir][1] # 0
     
-    # Y 축. 위랑 동일.
-    for j in range(n - 1):
-        y += dir
-        snail[y][x] = num
-        num -= 1
-        
-    dir = -dir
-    n -= 1
-
-
-for i in range(len(snail)):
-    for j in range(len(snail)):
-        if snail[i][j] == what:
-            print(i+1, j+1)
-        print(snail[i][j], end=' ')
+    
+    if next_y < 0 or next_x < 0 or next_y >= n or next_x >= n or \
+        snail[next_y][next_x] != 0:
+        curr_dir += 1
+        curr_dir = curr_dir % 4 # 0, 1, 2, 3
+    # 바뀐 방향대로 다시 값을 업데이트 -> curr_dir이 바뀌었을 수 있음
+    curr_y = curr_y + direction[curr_dir][0]
+    curr_x = curr_x + direction[curr_dir][1]
+    start -= 1
+# 답을 출력하기 위한 for문: 달팽이와 특정값의 좌표값
+for y in range(n):
+    for x in range(n):
+        print(snail[y][x], end=' ')
     print()
+print(solution[0], solution[1]) # 좌표값 출력
