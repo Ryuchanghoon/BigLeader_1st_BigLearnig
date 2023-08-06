ccd148140c7fd5a6978fd9a6ccc70780ee2418f4
류창훈 <fbckdgns3@naver.com>
2023-08-07 00:25:42 +0900

    [fix] 백준 2503번 파이썬 문제풀이

--- 알고리즘 문제/2503.py
+++ 알고리즘 문제/2503.py
@@ -1,44 +1,30 @@
 from itertools import permutations
 
-def base_strike(ball, guess, n):
-    strike = 0
-    for i in range(n):
-        if ball[i] == guess[i]:
-            strike += 1
-    return strike
-
-def base_ball(ball, guess, n):
-    ball = 0
-    for i in range(n):
-        if ball[i] in guess and ball[i] != guess[i]:
-            ball += 1
-    return ball
-
-def solve():
-    n = int(input())
-    base = []
-    for _ in range(n):
-        a, b, c = map(int, input().split())
-        base.append((str(a), b, c))
-
-    possible_numbers = list(permutations([str(i) for i in range(1, 10)], 3))
-    answer = 0
-
-    for number in possible_numbers:
-        is_possible = True
-
-        for baseball in base:
-            a, b, c = baseball
-            ball = str(ball)
-
-            if base_strike(ball, number, 3) != strike or base_ball(ball, number, 3) != ball_count:
-                is_possible = False
-                break
-
-        if is_possible:
-            answer += 1
-
-    print(answer)
 
 
-solution()
+num_list = list(permutations(range(1, 10), 3))
+
+n = int(input())
+
+for i in range(n):
+    say, strike, ball = map(int, input().split())
+    say = list(map(int, str(say)))
+
+    new_num_list = []
+
+    for num in num_list:
+        strike_count = 0
+        ball_count = 0
+
+        for a, b in zip(num, say):
+            if a == b:
+                strike_count += 1
+            elif a in say:
+                ball_count += 1
+
+        if strike_count == strike and ball_count == ball:
+            new_num_list.append(num)
+
+    num_list = new_num_list
+
+print(len(num_list))
